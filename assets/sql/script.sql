f51a99b2278669e56f07cb80e9d22275db82dc6c
KevsRaza <krazafindralanto@gmail.com>
2024-02-24 11:54:27 +0300

    Maj design

--- assets/sql/script.sql
+++ assets/sql/script.sql
@@ -2,12 +2,12 @@ CREATE DATABASE caisse;
 USE caisse;
 
 CREATE TABLE categorie (
-    id_cat int primary key,
+    id_cat int primary key auto_increment,
     nom varchar(50)
 );
 
 CREATE TABLE mouvement(
-    id_mouv int primary key,
+    id_mouv int primary key auto_increment,
     prix int,
     date date,
     id_cat int,
---
+++ categorie.php
@@ -0,0 +1,22 @@
+<?php
+    include("./traitement/fonction.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catégories</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+</head>
+<body>
+    <?php include("./link/navbar.php") ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
